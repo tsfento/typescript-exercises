2ca409472bfe2279d1217255443026ad39470c8a
Tyler Fenton <tsfento@gmail.com>
2023-09-24 20:48:10 -0500

    init commit

---
+++ 01-basic-types/main.js
@@ -0,0 +1,6 @@
+var studentName;
+var totalCourses;
+studentName = 'Tyler';
+totalCourses = 1;
+console.log(studentName);
+console.log(totalCourses);
---
+++ 01-basic-types/main.ts
@@ -0,0 +1,8 @@
+// let studentName: string;
+// let totalCourses: number;
+
+// studentName = 'Tyler';
+// totalCourses = 1;
+
+// console.log(studentName);
+// console.log(totalCourses);
