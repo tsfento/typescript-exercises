f93a043bff59e280cf30b9267165d96f2ee666b6
Tyler Fenton <tsfento@gmail.com>
2023-09-25 07:48:32 -0500

    Completed Exercise 4: Functions & Types

---
+++ exercise-04-functions-and-types/main.js
@@ -0,0 +1,4 @@
+function greet(name) {
+    return "Greetings, ".concat(name, "!");
+}
+console.log(greet('Tyler'));
---
+++ exercise-04-functions-and-types/main.ts
@@ -0,0 +1,6 @@
+// // Declared function with explicit return type
+// function greet(name: string): string {
+//     return `Greetings, ${name}!`;
+// }
+
+// console.log(greet('Tyler'));
