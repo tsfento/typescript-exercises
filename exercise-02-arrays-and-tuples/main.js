6a86f868b1ad42dc48b952d4d0e4d8022bce5bca
Tyler Fenton <tsfento@gmail.com>
2023-09-25 07:36:35 -0500

    Completed Exercise 02: Arrays & Tuples

---
+++ exercise-02-arrays-and-tuples/main.js
@@ -0,0 +1,6 @@
+var subjects;
+var student;
+subjects = ['HTML', 'CSS', 'Javascript'];
+student = ['Tyler', 42];
+console.log(subjects);
+console.log(student);
---
+++ exercise-02-arrays-and-tuples/main.ts
@@ -0,0 +1,8 @@
+// let subjects: string[];
+// let student: [string, number];
+
+// subjects = ['HTML', 'CSS', 'Javascript'];
+// student = ['Tyler', 42];
+
+// console.log(subjects);
+// console.log(student);
